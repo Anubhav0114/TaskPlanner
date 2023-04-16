828c3f1c0b4b87ef2921300512f6511037dda881
nitesh-dev <niteshdev547@gmail.com>
2023-04-16 11:35:48 +0530

    basic app structure completed

---
+++ app/src/main/java/com/example/taskplanner/fragments/ProjectFragment.kt
@@ -0,0 +1,28 @@
+package com.example.taskplanner.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskplanner.R
+import com.example.taskplanner.databinding.FragmentProjectBinding
+
+
+class ProjectFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentProjectBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProjectBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
