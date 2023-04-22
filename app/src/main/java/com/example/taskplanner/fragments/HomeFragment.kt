07bac1a7bd78cdf7ed936340afe7201f84abbee2
softweredevloper <ssayam200@gmail.com>
2023-04-23 01:42:39 +0530

    Alert Box Added

--- app/src/main/java/com/example/taskplanner/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/taskplanner/fragments/HomeFragment.kt
@@ -1,14 +1,18 @@
 package com.example.taskplanner.fragments
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.taskplanner.ProjectApplication
+import com.example.taskplanner.R
 import com.example.taskplanner.databinding.FragmentHomeBinding
 import com.example.taskplanner.viewmodel.MainActivityViewModel
 import com.example.taskplanner.viewmodel.MainActivityViewModelFactory
@@ -49,6 +53,33 @@ class HomeFragment : Fragment() {
         }
 
 
+        binding.newProjectBtn.setOnClickListener {
+            // Alert Box
+            dialog()
+        }
         //findNavController().navigate(R.id.action_homeFragment_to_projectFragment)
+    }
+
+    private fun dialog(){
+        val dialogView = LayoutInflater.from(contextApp).inflate(R.layout.custom_dialog, null)
+        val builder = AlertDialog.Builder(contextApp)
+        builder.setView(dialogView)
+
+        val dialog = builder.create()
+
+        dialogView.findViewById<Button>(R.id.dialog_ok)?.setOnClickListener {
+            // handle OK button click
+            Toast.makeText(contextApp,"Add Project To Db",Toast.LENGTH_LONG).show()
+            dialog.dismiss()
+        }
+
+        dialogView.findViewById<Button>(R.id.dialog_cancel)?.setOnClickListener {
+            // handle Cancel button click
+            Toast.makeText(contextApp,"Close It",Toast.LENGTH_LONG).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
+
     }
 }
