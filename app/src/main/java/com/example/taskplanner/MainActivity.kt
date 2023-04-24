149dd43936839fad7a73853f4be094497c072968
nitesh-dev <niteshdev547@gmail.com>
2023-04-24 15:46:23 +0530

    nav drawer shifted to main activity but item click not working

--- app/src/main/java/com/example/taskplanner/MainActivity.kt
+++ app/src/main/java/com/example/taskplanner/MainActivity.kt
@@ -1,14 +1,74 @@
 package com.example.taskplanner
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.taskplanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val appLink = "https://play.google.com/store/apps/details?id=com.flaxstudio.drawon"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        // Hello sayam
+        binding.navView.setNavigationItemSelectedListener { menuItem ->
+
+            when(menuItem.itemId){
+                R.id.notification_item -> {
+                    Toast.makeText(applicationContext, "Clicked", Toast.LENGTH_LONG).show()
+                }
+                R.id.about_item -> {
+                    Toast.makeText(applicationContext, "Clicked", Toast.LENGTH_LONG).show()
+                }
+                R.id.feedback_item -> {
+                    val intent = Intent().apply{
+                        action = Intent.ACTION_SENDTO
+                        data = Uri.parse("mailto:")
+                        putExtra(Intent.EXTRA_EMAIL, arrayOf("flaxstudiohelp@gmail.com"))
+                        putExtra(Intent.EXTRA_SUBJECT, "Tell about our application")
+                    }
+                    startActivity(Intent.createChooser(intent, "Send Email"))
+                }
+                R.id.rating_item -> {
+                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(appLink))
+                    startActivity(browserIntent)
+                }
+                R.id.shareItem -> {
+                    val sendIntent : Intent = Intent().apply {
+                        action = Intent.ACTION_SEND
+                        putExtra(Intent.EXTRA_TEXT , "Hey, I just made a really cool Task using Task Planner App .You should also download this amazing App.")
+                        type = "text/plain"
+                    }
+                    val shareIntent = Intent.createChooser(sendIntent , "Share Task Planner to your Friends")
+                    startActivity(shareIntent)
+                }
+                R.id.moreApps_item -> {
+                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://flax-studio.vercel.app"))
+                    startActivity(browserIntent)
+                }
+            }
+
+            true
+        }
+
+
+    }
+
+
+    fun openNavDrawer(){
+        binding.drawerLayout.open()
     }
 }
--- app/src/main/java/com/example/taskplanner/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/taskplanner/fragments/HomeFragment.kt
@@ -1,34 +1,28 @@
 package com.example.taskplanner.fragments
 
-import android.app.Activity
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.example.taskplanner.MainActivity
 import com.example.taskplanner.ProjectApplication
 import com.example.taskplanner.R
 import com.example.taskplanner.databinding.FragmentHomeBinding
 import com.example.taskplanner.viewmodel.MainActivityViewModel
 import com.example.taskplanner.viewmodel.MainActivityViewModelFactory
-import com.google.android.play.core.review.ReviewManagerFactory
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
-    private  lateinit var toggle:ActionBarDrawerToggle
     private val mainActivityViewModel: MainActivityViewModel by activityViewModels {
         MainActivityViewModelFactory(
             (requireActivity().application as ProjectApplication).projectRepository,
@@ -36,7 +30,6 @@ class HomeFragment : Fragment() {
         )
     }
     private lateinit var contextApp: Context
-    private val appLink = "https://play.google.com/store/apps/details?id=com.flaxstudio.drawon"
 
 
     override fun onCreateView(
@@ -52,51 +45,11 @@ class HomeFragment : Fragment() {
 
         contextApp = requireContext()
 
-        toggle = ActionBarDrawerToggle(contextApp as Activity?,binding.drawerLayout,R.string.open,R.string.close)
-        binding.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        binding.navButton.setOnClickListener {
-            binding.drawerLayout.open()
+        binding.navButton.setOnClickListener{
+            (activity as MainActivity).openNavDrawer()
         }
 
-        binding.navView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.notification_item -> {
-                    Toast.makeText(contextApp, "Clicked", Toast.LENGTH_LONG).show()
-                }
-                R.id.about_item -> {
-                    Toast.makeText(contextApp, "Clicked", Toast.LENGTH_LONG).show()
-                }
-                R.id.feedback_item -> {
-                    val intent = Intent().apply{
-                        action = Intent.ACTION_SENDTO
-                        data = Uri.parse("mailto:")
-                        putExtra(Intent.EXTRA_EMAIL, arrayOf("flaxstudiohelp@gmail.com"))
-                        putExtra(Intent.EXTRA_SUBJECT, "Tell about our application")
-                    }
-                    startActivity(Intent.createChooser(intent, "Send Email"))
-                }
-                R.id.rating_item -> {
-                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(appLink))
-                    startActivity(browserIntent)
-                }
-                R.id.shareItem -> {
-                    val sendIntent : Intent = Intent().apply {
-                        action = Intent.ACTION_SEND
-                        putExtra(Intent.EXTRA_TEXT , "Hey, I just made a really cool Task using Task Planner App .You should also download this amazing App.")
-                        type = "text/plain"
-                    }
-                    val shareIntent = Intent.createChooser(sendIntent , "Share Task Planner to your Friends")
-                    startActivity(shareIntent)
-                }
-                R.id.moreApps_item -> {
-                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://flax-studio.vercel.app"))
-                    startActivity(browserIntent)
-                }
-            }
-            true
-        }
+
         mainActivityViewModel.getAllProjectsTask {
             Toast.makeText(contextApp, "Room Working", Toast.LENGTH_SHORT).show()
         }
@@ -109,13 +62,6 @@ class HomeFragment : Fragment() {
         //findNavController().navigate(R.id.action_homeFragment_to_projectFragment)
     }
 
-    @Suppress("DEPRECATION")
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
     private fun dialog(){
         val dialogView = LayoutInflater.from(contextApp).inflate(R.layout.add_project_dialog, null)
         val builder = AlertDialog.Builder(contextApp)
