fb630ba3edf7452c943d3ebbcd9a44b0787fb5db
Anubhav pathak <88669266+Anubhav0114@users.noreply.github.com>
2023-04-24 16:11:04 +0530

    Half of notification feature implemented

--- app/src/main/java/com/example/taskplanner/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/taskplanner/fragments/HomeFragment.kt
@@ -1,27 +1,33 @@
 package com.example.taskplanner.fragments
 
-import android.app.Activity
+import android.app.*
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.taskplanner.ProjectApplication
 import com.example.taskplanner.R
+import com.example.taskplanner.Task
 import com.example.taskplanner.databinding.FragmentHomeBinding
 import com.example.taskplanner.viewmodel.MainActivityViewModel
 import com.example.taskplanner.viewmodel.MainActivityViewModelFactory
-import com.google.android.play.core.review.ReviewManagerFactory
 
 
 class HomeFragment : Fragment() {
@@ -47,6 +53,7 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -107,6 +114,80 @@ class HomeFragment : Fragment() {
             dialog()
         }
         //findNavController().navigate(R.id.action_homeFragment_to_projectFragment)
+
+
+
+        // Alarm manager for notification
+        var taskOne : Task = Task(SystemClock.elapsedRealtime() + 60 * 1000, SystemClock.elapsedRealtime() + 120 * 1000 , "TaskOne" , "DescTaskOne" , 0)
+//        var taskTwo : Task = Task(4484884, 5998595 , "TaskOne" , "DescTaskOne" , 44484)
+//        var taskThree : Task = Task(4484884, 5998595 , "TaskOne" , "DescTaskOne" , 44484)
+
+        // List of tasks
+        val taskList = ArrayList<Task>()
+        taskList.add(taskOne)
+
+
+        // Loop through the task list and schedule notifications for each task
+        val alarmManager1 = contextApp?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        for (task in taskList) {
+            // Create an intent for the start time notification with a unique request code
+            val startTimeIntent = Intent(contextApp, StartTimeReceiver::class.java)
+            val startTimePendingIntent = PendingIntent.getBroadcast(
+                contextApp, 0, startTimeIntent, PendingIntent.FLAG_IMMUTABLE
+            )
+
+            // Create an intent for the end time notification with a unique request code
+            val endTimeIntent = Intent(contextApp, EndTimeReceiver::class.java)
+            val endTimePendingIntent = PendingIntent.getBroadcast(
+                contextApp, 123, endTimeIntent, PendingIntent.FLAG_IMMUTABLE
+            )
+
+            // Schedule the start time notification
+            alarmManager1?.setExactAndAllowWhileIdle(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                task.startTime,
+                startTimePendingIntent
+            )
+
+            // Schedule the end time notification
+            alarmManager1?.setExactAndAllowWhileIdle(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                task.endTime,
+                endTimePendingIntent
+            )
+        }
+
+
+
+
+
+
+
+
+
+//        val alarmManager = contextApp?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//
+//        val intent =  Intent(contextApp, StartTimeReceiver::class.java)
+//        Log.i("HomeFragment" , "Setting the pending intent")
+//        val pendingIntent : PendingIntent = PendingIntent.getBroadcast(
+//            requireContext(), 0, intent,
+//            PendingIntent.FLAG_IMMUTABLE
+//        )
+//
+//
+//        alarmManager.setExactAndAllowWhileIdle(
+//            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+//            SystemClock.elapsedRealtime() + 60 * 1000,
+//            pendingIntent
+//        )
+
+
+
+
+
+
+
+
     }
 
     @Suppress("DEPRECATION")
@@ -140,3 +221,62 @@ class HomeFragment : Fragment() {
 
     }
 }
+
+class StartTimeReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // Show notification for start time
+        var channelId = "CHANNEL_ID"
+
+        val notificationBuilder = NotificationCompat.Builder(context, channelId)
+            .setContentTitle("Task Started")
+            .setContentText("Your task has started now")
+            .setSmallIcon(R.drawable.icon_task_done)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        Log.i("HomeFragment" , "This is the notification Builder class")
+
+
+        // Show the notification
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, notificationBuilder.build())
+
+    }
+}
+
+
+
+class EndTimeReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // Show notification for end time
+
+        var channelId = "CHANNEL_ID"
+        val notificationBuilder = NotificationCompat.Builder(context, channelId)
+            .setContentTitle("Task Ended")
+            .setContentText("Did you finished your task")
+            .setSmallIcon(R.drawable.icon_not_finished)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+
+// Show the notification
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(123, notificationBuilder.build())
+
+    }
+}
+
