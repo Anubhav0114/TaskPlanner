ef8499f3d908779b86a848a18f0ca630886ed3ee
nitesh-dev <niteshdev547@gmail.com>
2023-04-24 13:51:16 +0530

    Custom chip added

--- app/src/main/java/com/example/taskplanner/utils/Utils.kt
+++ app/src/main/java/com/example/taskplanner/utils/Utils.kt
@@ -8,12 +8,7 @@ val Number.toPx get() = TypedValue.applyDimension(
     this.toFloat(),
     Resources.getSystem().displayMetrics).toInt()
 
-class Vector2{
-    var x = 0f
-    var y = 0f
-
-    fun setValue(x: Float, y: Float){
-        this.x = x
-        this.y = y
-    }
-}
+val Number.spToPx get() = TypedValue.applyDimension(
+    TypedValue.COMPLEX_UNIT_SP,
+    this.toFloat(),
+    Resources.getSystem().displayMetrics).toInt()
